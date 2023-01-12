6cd1c1e0de44e200d39fe2f1f07e935c7df622f7
WallochMatt <matthewrwalloch@gmail.com>
2023-01-12 14:42:10 -0600

    feat: Created a Student class and instantiated a Student object

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -2,6 +2,8 @@
 to determine how much money someone has made. Overtime is NOT taken into account.
 */
 
+using Intro;
+
 int hoursWorked;
 hoursWorked= 35;
 
@@ -11,5 +13,18 @@ payRate = 7.25;
 double totalPay;
 totalPay=hoursWorked*payRate;
 
-Console.WriteLine(totalPay);
+//Console.WriteLine(totalPay);
+//Console.ReadLine();
+
+/*
+Your next C# task will be to create a class with a couple member variables.
+Go ahead and create a class called 'Student'. 
+This can be done in the project that you used for the previous task, or a brand new project if you want.
+*/
+
+Student mySelf = new Student();
+mySelf.name = "Matthew Walloch";
+mySelf.overallGradePercentage= 108.36;
+
+Console.WriteLine("Name: " + mySelf.name + "\n" + "Overall Grade: " + mySelf.overallGradePercentage + "%");
 Console.ReadLine();
---
+++ Intro/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    internal class Student
+    {
+        public string? name;
+
+        public double overallGradePercentage;
+
+
+
+
+    }
+}
