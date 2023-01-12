5b81b23760ed7310939c652da4375602243bd3ac
WallochMatt <matthewrwalloch@gmail.com>
2023-01-12 14:00:41 -0600

    feat: Created project + Created a simple pay calculator

---
+++ Intro/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Your first C# task is to write some code using variables, data types, and operators to create a "pay calculator" 
+to determine how much money someone has made. Overtime is NOT taken into account.
+*/
+
+int hoursWorked;
+hoursWorked= 35;
+
+double payRate;
+payRate = 7.25;
+
+double totalPay;
+totalPay=hoursWorked*payRate;
+
+Console.WriteLine(totalPay);
+Console.ReadLine();
